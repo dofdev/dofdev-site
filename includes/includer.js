719fe903b12cf25b9858388c0ef80d1dc17a90b8
spatialfree <ethan@dofdev.org>
2020-11-14 17:21:26 -0800

    gifs only load in on view, and padded the h1

--- includes/includer.js
+++ includes/includer.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
   $("#navElement").load("/includes/nav.html");
   $("#footerElement").load("/includes/footer.html");
+
+  inView('.gif')
+    .on('enter', el => {
+      $(el).css('background-image', 'url(' + $(el).attr('href') + ')')
+    })
+    .on('exit', el => {
+      $(el).css('background-image', '')
+    })
 });
