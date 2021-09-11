5446a81008a637e35301929aade9d7a3a138c058
spatialfree <cadetmerchant@gmail.com>
2021-09-10 21:28:49 -0400

    free to the world?

--- includes/includer.js
+++ includes/includer.js
@@ -1,12 +1,25 @@
 $(document).ready(function () {
-  $("#navElement").load("/includes/nav.html");
+  $("#navElement").load("/includes/nav.xml", function () {
+    $("#nav [href]").each(function () {
+      if (window.location.href.includes(this.href)) {
+        $(this).addClass("active");
+      }
+    });
+  });
+    
   $("#footerElement").load("/includes/footer.html");
 
+  // $("#feeds").load("feeds.php", { limit: 25 }, function () {
+  //   alert("The last 25 entries in the feed have been loaded");
+  // });
+
+  // go to console in browser and type: document.designMode = "on"
+  
   inView('.gif')
-    .on('enter', el => {
-      $(el).css('background-image', 'url(' + $(el).attr('href') + ')')
-    })
-    .on('exit', el => {
-      $(el).css('background-image', '')
-    })
+  .on('enter', el => {
+    $(el).css('background-image', 'url(' + $(el).attr('href') + ')')
+  })
+  .on('exit', el => {
+    $(el).css('background-image', '')
+  })
 });
