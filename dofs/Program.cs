e1398dec33d41d616003be2bad4bca63b28113d7
spatialfree <cadetmerchant@gmail.com>
2021-11-02 17:09:41 -0400

    fixed license issues

--- dofs/Program.cs
+++ dofs/Program.cs
@@ -100,7 +100,7 @@ namespace DOFS
         html += $"<div class='l'>{lastUpdated.ToString("yyyy.MM.dd")}</div>";
         html += "</div>";
         html += $"<h2 style='color: var(--white);text-decoration: none;'>&deg;{dof.name}</h2>";
-        html += "<a href='https://unlicense.org' style='text-decoration: underline; text-align: right; display: block; margin-bottom: 0.5em;'>unlicense</a>";
+        html += "<a class='fineprint' href='/dofs' style='text-decoration: underline; text-align: right; display: block; margin-bottom: 0.5em;'>MIT License | Copyright © 2018-2021 dofdev</a>";
         html += $"{content}";
         html += $"";
         html += "<div id='footerElement'></div>";
