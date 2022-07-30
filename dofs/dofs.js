794c61e37d6db83dead23864506431502b0d5278
spatialfree <cadetmerchant@gmail.com>
2022-07-30 17:19:17 -0400

    dof wording

--- dofs/dofs.js
+++ dofs/dofs.js
@@ -2,8 +2,8 @@ const dofTree = [
   'stretch-cursor|spatial control using hand distance',
   'color-cube|color picker & indexer xyz >>> rgb',
   'oriel|volumetric graphical control element',
-  'orbital-view|inversely viewed center of interest',
-  'offset-cursor|1:1 spatial analog of the mouse',
+  'orbital-view|inversely viewed point of interest',
+  'offset-cursor|1:x spatial analog of the mouse',
   'fullstick|joystick with full range of motion',
   'trackballer|freely spin with a flick',
   'quadraticizer|a quadratic curve shaper',
@@ -11,4 +11,4 @@ const dofTree = [
   'cubic-flow|embody a cubic bezier curve',
   'reach-cursor|project from a placed anchor',
   'touch-xr|touch based spatial controls',
-];
+]
